a16e8de2814cbc04cbbab7355de6abdac4fd2378
Pallat Anchaleechamaikorn <pallat.anc@cjexpress.co.th>
2022-02-24 13:33:45 +0700

    refactor if for any number return itself as string

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -6,14 +6,10 @@ func Say(n int) string {
 	if n == 5 {
 		return "Bar"
 	}
-	if n == 4 {
-		return strconv.Itoa(n)
-	}
+
 	if n == 3 {
 		return "Foo"
 	}
-	if n == 2 {
-		return strconv.Itoa(n)
-	}
+
 	return strconv.Itoa(n)
 }
